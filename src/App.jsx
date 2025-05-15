f58492d728c8987785926afaa15db84fe3a67d78
drobiss <brezinadavid93@icloud.com>
2025-05-15 20:10:54 +0200

    *

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span>Movies</span> You'll Enjoy Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+};
+
+export default App;
