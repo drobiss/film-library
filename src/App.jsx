441caff4adfecbf949cde662d43b06a6ea050647
drobiss <brezinadavid93@icloud.com>
2025-05-19 07:50:28 +0200

    *

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ const App = () => {
       <div className="wrapper">
         <header>
           <h1>
-            Find <span>Movies</span> You'll Enjoy Without the Hassle
+            <img src="./hero-img.png" alt="" />
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
           </h1>
         </header>
+
+        <p>Search</p>
       </div>
     </main>
   );
